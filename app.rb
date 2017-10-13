54b896e50f06cce20a7d91601eda62ea0dc63748
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-13 18:20:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,19 +5,19 @@ class App < Sinatra::Base
 get '/name' do
 
 "My  name is "
- erb :'name/index.html.erb'
+
 end
 
 get '/hometown' do
 "My  hometown is "
 
- erb :'hometown/index.html.erb'
+
 end
 
 get '/favorite-song' do
   "My  favorite song is "
 
-   erb :'favorite-song/index.html.erb'
+  
 end
 
 
