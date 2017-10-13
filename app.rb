c0156a3e3b78a0a5cc030b84e5b93167635715e0
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-13 18:24:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+
+"My name is Tom McGlaughlin."
+
+end
+
+get '/hometown' do
+
+"My hometown is Philadelphia."
+
+
+end
+
+get '/favorite-song' do
+  "My favorite song is Beethoven's 7th Symphony. "
+
+
+end
+
+
 end
