c47e6a2b04b9a4c860e271954582f77a128cf312
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-13 18:24:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,18 +4,19 @@ class App < Sinatra::Base
 
 get '/name' do
 
-"My  name is "
+"My  name is Tom McGlaughlin"
 
 end
 
 get '/hometown' do
-"My  hometown is "
+
+"My  hometown is Philadelphia."
 
 
 end
 
 get '/favorite-song' do
-  "My  favorite song is "
+  "My favorite song is Beethoven's 7th Symphony. "
 
 
 end
